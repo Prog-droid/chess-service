53e7065faa8750caf03172ce958dc8d11c4e68b3
Prog-droid <Jacob.Connor@live.co.uk>
2022-09-17 20:10:43 +0100

    Java files setup

---
+++ src/test/java/com/example/chessservice/ChessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chessservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
